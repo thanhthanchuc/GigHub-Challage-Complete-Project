8c91853bca82191024c37a3aa4ca3d5949dd2363
toiye <toiye@DESKTOP-BJB9GPG>
2018-08-04 18:34:12 +0700

    Creating Domain Classes

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venua { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
